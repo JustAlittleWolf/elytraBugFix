400521514ce47688c00c0e855094c27d013083a0
Wolfi <wolfi@wolfislaptop>
2026-03-28 10:28:08 +0100

    feat: migrate to mojmap

--- src/main/java/me/wolfii/fixelytrabug/mixin/PlayerEntityMixin.java
+++ src/main/java/me/wolfii/fixelytrabug/mixin/PlayerEntityMixin.java
@@ -1,6 +1,6 @@
 package me.wolfii.fixelytrabug.mixin;
 
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,15 +8,15 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @SuppressWarnings("DataFlowIssue")
-@Mixin(PlayerEntity.class)
+@Mixin(Player.class)
 public class PlayerEntityMixin {
-    @Inject(method = "checkGliding", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "tryToStartFallFlying", at = @At("HEAD"), cancellable = true)
     private void injectedStartFallFlying(CallbackInfoReturnable<Boolean> cir) {
-        if (((PlayerEntity) (Object) this).isSneaking()) return;
-        if (((PlayerEntity) (Object) this).isGliding()) return;
-        if (((PlayerEntity) (Object) this).isInLava()) return;
+        if (((Player) (Object) this).isShiftKeyDown()) return;
+        if (((Player) (Object) this).isFallFlying()) return;
+        if (((Player) (Object) this).isInLava()) return;
         double offset = 4.0 / 16.0;
-        if (((PlayerEntity) (Object) this).isSprinting()) offset = 12.0 / 16.0;
+        if (((Player) (Object) this).isSprinting()) offset = 12.0 / 16.0;
         if (this.doesCollideY(offset) && this.doesCollideY(-offset)) {
             cir.setReturnValue(false);
         }
@@ -24,6 +24,6 @@ public class PlayerEntityMixin {
 
     @Unique
     private boolean doesCollideY(double offsetY) {
-        return !((PlayerEntity) (Object) this).doesNotCollide(0, offsetY, 0);
+        return !((Player) (Object) this).isFree(0, offsetY, 0);
     }
 }
